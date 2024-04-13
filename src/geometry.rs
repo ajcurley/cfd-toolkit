08a3fb0b012f80198dbb082a6063a702fc64c46b
Alex Curley <=>
2024-04-13 11:03:06 -0400

    Add Sphere and Ray

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,8 +1,12 @@
 pub mod aabb;
+pub mod ray;
+pub mod sphere;
 pub mod triangle;
 pub mod vector;
 
 // Re-exports
 pub use aabb::Aabb;
+pub use ray::Ray;
+pub use sphere::Sphere;
 pub use triangle::Triangle;
 pub use vector::Vector;
