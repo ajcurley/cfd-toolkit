8fff64c8ca847780b6ab10171b0256502abd62ae
Alex Curley <=>
2024-05-18 10:25:56 -0400

    Remove mesh module

--- src/mesh/errors.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Debug, Clone)]
-pub struct NonManifoldError;
-
-impl std::fmt::Display for NonManifoldError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "non-manifold edge found")
-    }
-}
