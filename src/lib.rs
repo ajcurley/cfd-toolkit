6164eadf572151b4bdf1d5a86f7064ea2dfd9757
Alex Curley <=>
2024-05-18 10:25:56 -0400

    Remove mesh module

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use pyo3::prelude::*;
 
 pub mod geometry;
-pub mod mesh;
 
 #[pymodule]
 fn _cfd_toolkit_core(m: &Bound<'_, PyModule>) -> PyResult<()> {
@@ -11,16 +10,6 @@ fn _cfd_toolkit_core(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<geometry::Triangle>()?;
     m.add_class::<geometry::Ray>()?;
     m.add_class::<geometry::Vector>()?;
-
-    // Mesh
-    m.add_class::<mesh::Face>()?;
-    m.add_class::<mesh::FaceHandle>()?;
-    m.add_class::<mesh::HalfEdge>()?;
-    m.add_class::<mesh::HalfEdgeHandle>()?;
-    m.add_class::<mesh::Patch>()?;
-    m.add_class::<mesh::PatchHandle>()?;
-    m.add_class::<mesh::Vertex>()?;
-    m.add_class::<mesh::VertexHandle>()?;
 
     Ok(())
 }
--- src/mesh.rs
+++
@@ -1,9 +0,0 @@
-pub mod errors;
-pub mod half_edge;
-pub mod io;
-pub mod wavefront;
-
-// Re-exports
-pub use errors::*;
-pub use half_edge::*;
-pub use io::*;
