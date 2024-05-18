f51d75f31d54b281f14d42720a7eeb0e4cc305cf
Alex Curley <=>
2024-05-18 12:57:08 -0400

    Improve imports/exports. Add Python mesh module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use meshx::geometry;
 use pyo3::prelude::*;
 
+pub mod mesh;
+
 #[pymodule]
 fn _cfd_toolkit_core(m: &Bound<'_, PyModule>) -> PyResult<()> {
     // Geometry
@@ -9,6 +11,9 @@ fn _cfd_toolkit_core(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<geometry::Triangle>()?;
     m.add_class::<geometry::Ray>()?;
     m.add_class::<geometry::Vector3>()?;
+
+    // Mesh
+    m.add_class::<mesh::SurfaceMesh>()?;
 
     Ok(())
 }
