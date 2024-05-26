1033a43965f010683ac1f21b1ce6914b9339f2ef
Alex Curley <=>
2024-05-26 10:58:07 -0400

    Add feature edge calculation

--- src/mesh.rs
+++ src/mesh.rs
@@ -1,4 +1,6 @@
+pub mod common;
 pub mod surface;
 
 // Re-exports
+pub use common::Edge;
 pub use surface::SurfaceMesh;
--- src/mesh/surface.rs
+++ src/mesh/surface.rs
@@ -2,6 +2,8 @@ use meshx::mesh::half_edge::HeMesh;
 use pyo3::exceptions::PyIOError;
 use pyo3::prelude::*;
 
+use crate::mesh::Edge;
+
 #[pyclass]
 #[derive(Debug, Clone)]
 pub struct SurfaceMesh {
@@ -27,5 +29,38 @@ impl SurfaceMesh {
     /// Check if the surface mesh is consistently oriented
     pub fn is_consistent(&self) -> bool {
         self.data.is_consistent()
+    }
+
+    /// Compute the feature edges.
+    pub fn feature_edges(&self, angle: f64) -> Vec<Edge> {
+        let mut edges = vec![];
+
+        for (i, j) in self.data.feature_edges(angle) {
+            let mut patches = vec![];
+
+            let half_edge = self.data.half_edge(i);
+            let face = half_edge.face();
+            let next = half_edge.next();
+            let p = half_edge.origin();
+            let q = self.data.half_edge(next).origin();
+
+            if let Some(patch) = self.data.face(face).patch() {
+                patches.push(patch);
+            }
+
+            let half_edge = self.data.half_edge(j);
+            let face = half_edge.face();
+
+            if let Some(patch) = self.data.face(face).patch() {
+                if !patches.contains(&patch) {
+                    patches.push(patch);
+                }
+            }
+
+            let edge = Edge::new(p, q, patches);
+            edges.push(edge);
+        }
+
+        edges
     }
 }
