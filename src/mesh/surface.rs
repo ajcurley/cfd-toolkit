ee5b0343baa4734e4f77fc728109dcb00b9feb8a
Alex Curley <=>
2024-06-06 06:34:19 -0400

    Refactor feature_edges for simplicity

--- src/mesh/surface.rs
+++ src/mesh/surface.rs
@@ -68,6 +68,22 @@ impl SurfaceMesh {
         self.data.n_half_edges()
     }
 
+    /// Get the edges
+    pub fn edges(&self) -> Vec<Edge> {
+        (0..self.n_edges()).map(|i| self.edge(i)).collect()
+    }
+
+    /// Get an edge by index
+    pub fn edge(&self, index: usize) -> Edge {
+        let half_edge = self.data.half_edge(index);
+        let next = self.data.half_edge(half_edge.next());
+        let face = self.data.face(half_edge.face());
+        let p = half_edge.origin();
+        let q = next.origin();
+        let patch = face.patch();
+        Edge::new(p, q, patch)
+    }
+
     /// Get the number of patches
     pub fn n_patches(&self) -> usize {
         self.data.n_patches()
@@ -111,30 +127,6 @@ impl SurfaceMesh {
         self.data.orient();
     }
 
-    /// Compute the feature edges. Each edge represents one-side of a pair
-    /// of edges belonging the two to adjacent faces.
-    pub fn feature_edges(&self, angle: f64) -> Vec<(Edge, Edge)> {
-        let mut edges = vec![];
-
-        for (i, j) in self.data.feature_edges(angle) {
-            let half_edge = self.data.half_edge(i);
-            let next = half_edge.next();
-            let p = half_edge.origin();
-            let q = self.data.half_edge(next).origin();
-
-            let face = self.data.face(half_edge.face());
-            let edge_i = Edge::new(p, q, face.patch());
-
-            let half_edge = self.data.half_edge(j);
-            let face = self.data.face(half_edge.face());
-            let edge_j = Edge::new(p, q, face.patch());
-
-            edges.push((edge_i, edge_j));
-        }
-
-        edges
-    }
-
     /// Merge the surface mesh naively. This strictly adds the contents
     /// of the two meshes but does not merge any duplicates.
     pub fn merge(&mut self, other: &SurfaceMesh) {
@@ -159,5 +151,11 @@ impl SurfaceMesh {
     /// is defined by its faces.
     pub fn components(&self) -> Vec<Vec<usize>> {
         self.data.components()
+    }
+
+    /// Compute the feature edges. Each edge represents one-side of a pair
+    /// of edges belonging the two to adjacent faces.
+    pub fn feature_edges(&self, angle: f64) -> Vec<(usize, usize)> {
+        self.data.feature_edges(angle)
     }
 }
